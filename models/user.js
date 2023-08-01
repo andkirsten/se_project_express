60576e497b28a5230aed4cba8202b3187bb094c4
andkirsten <and.kirsten@gmail.com>
2023-08-01 09:42:59 +0900

    userSchema updated

--- models/user.js
+++ models/user.js
@@ -19,6 +19,23 @@ const userSchema = new mongoose.Schema({
       message: (props) => `${props.value} is not a valid URL`,
     },
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator: function validateEmail(value) {
+        return validator.isEmail(value);
+      },
+    },
+    message: (props) => `${props.value} is not a valid email`,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters"],
+    select: false,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
